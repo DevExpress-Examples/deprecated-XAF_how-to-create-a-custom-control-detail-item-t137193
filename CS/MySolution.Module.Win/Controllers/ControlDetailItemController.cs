b61cd88ce21b27be9b44b3f98a4741e771b1a0ef
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MySolution.Module.Win/Controllers/ControlDetailItemController.cs
@@ -0,0 +1,33 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Layout;
+using MySolution.Module.BusinessObjects;
+using System;
+using System.Windows.Forms;
+
+namespace MySolution.Module.Win.Controllers {
+    public class ControlDetailItemController : ViewController {
+        public ControlDetailItemController() {
+            TargetViewType = ViewType.DetailView;
+            TargetObjectType = typeof(Contact);
+        }
+        protected override void OnActivated() {
+            base.OnActivated();
+            ControlDetailItem item = ((DetailView)View).FindItem("MyButton") as ControlDetailItem;
+            if (item != null) {
+                if (item.Control != null) {
+                    item_ControlCreated(item, EventArgs.Empty);
+                }
+                else {
+                    item.ControlCreated += item_ControlCreated;
+                }
+            }
+        }
+        void item_ControlCreated(object sender, EventArgs e) {
+            (((ControlDetailItem)sender).Control as Button).Text = "Click me!";
+            (((ControlDetailItem)sender).Control as Button).Click += ButtonClickHandlingWinController_Click;
+        }
+        void ButtonClickHandlingWinController_Click(object sender, EventArgs e) {
+            throw new UserFriendlyException("Action from custom detail item was executed!");
+        }
+    }
+}
---
+++ VB/MySolution.Module.Web/Controllers/ControlDetailItemController.vb
@@ -0,0 +1,41 @@
+﻿Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Layout
+Imports DevExpress.ExpressApp.Web
+Imports DevExpress.Web
+Imports MySolution.Module.BusinessObjects
+Imports System
+
+Namespace MySolution.Module.Web.Controllers
+    Public Class ControlDetailItemController
+        Inherits ViewController
+
+        Public Sub New()
+            TargetViewType = ViewType.DetailView
+            TargetObjectType = GetType(Contact)
+        End Sub
+        Protected Overrides Sub OnActivated()
+            MyBase.OnActivated()
+            Dim item As ControlDetailItem = TryCast(CType(View, DetailView).FindItem("MyButton"), ControlDetailItem)
+            If item IsNot Nothing Then
+                If item.Control IsNot Nothing Then
+                    item_ControlCreated(item, EventArgs.Empty)
+                Else
+                    AddHandler item.ControlCreated, AddressOf item_ControlCreated
+                End If
+            End If
+        End Sub
+        Private Sub item_ControlCreated(ByVal sender As Object, ByVal e As EventArgs)
+            Dim button As ASPxButton = TryCast(DirectCast(sender, ControlDetailItem).Control, ASPxButton)
+            If button Is Nothing Then
+                Return
+            End If
+            button.ID = "MyButton"
+            button.Text = "Click me!"
+            AddHandler button.Click, AddressOf ButtonClickHandlingWebController_Click
+        End Sub
+
+        Private Sub ButtonClickHandlingWebController_Click(ByVal sender As Object, ByVal e As EventArgs)
+            WebWindow.CurrentRequestWindow.RegisterClientScript("ShowAlert", "alert('Action from custom detail item was executed!');")
+        End Sub
+    End Class
+End Namespace
